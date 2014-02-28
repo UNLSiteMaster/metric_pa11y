5051e81cbd5ca533514a6844b73f19a3ebd75ec7
Michael Fairchild <mfairchild365@gmail.com>
2014-02-28 13:26:17 -0600

    Add a description for the metric

---
+++ www/templates/html/Metric.tpl.php
@@ -0,0 +1,6 @@
+<p>
+    Pa11y can't catch all accessibility errors. It'll catch many of them, but you should do manual checking as well.
+</p>
+<p>
+    Only distinct <?php echo $context->options['standard'] ?> errors are being reported.  To locate and fix errors on your page, <a href="http://squizlabs.github.io/HTML_CodeSniffer/">install the code sniffer bookmarklet</a> and run it on your page.
+</p>
