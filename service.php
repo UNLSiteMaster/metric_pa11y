249672944e71bf107fc29bd7b8d4e6864b79820f
Michael Fairchild <mfairchild365@gmail.com>
2014-10-31 15:17:55 -0500

    prevent pa11y from stalling the queue
    Sometimes, pa11y gets hung up and stalls the queue for days until it is manually restarted.  I'm not sure why this is happening, as I can't reproduce the problem at all.  However, abstracting the pa11y logic out of the queue and into a separate request should fix this problem, because the http request will always time out after 30 seconds.

---
+++ service.php
@@ -0,0 +1,37 @@
+<?php
+require_once(__DIR__ . '/../../init.php');
+$metric_pa11y = \SiteMaster\Core\Plugin\PluginManager::getManager()->getPluginInfo('metric_pa11y');
+$options = $metric_pa11y->getOptions();
+
+function getResults($uri, $plugin_options)
+{
+    $command = $plugin_options['pa11y_path']
+        . ' -r json'
+        . ' -s ' . escapeshellarg($plugin_options["standard"])
+        . ' -c ' . escapeshellarg($plugin_options['html_codesniffer_url'] . 'HTMLCS.js');
+
+    $config_file = dirname(__DIR__) . '/config/pa11y.json';
+    if (file_exists($config_file)) {
+        $command .= ' --config ' . $config_file;
+    }
+
+    $command .= ' ' . escapeshellarg($uri);
+
+    /**
+     * Execute with a 25 second timeout (just under the default of 30).
+     * one or more of pa11y, phantomjs, or node have an issue where child process are not being killed and turn into zombies.
+     * $this->exec aims to curb that problem by manually setting a timeout.
+     */
+    $command_helper = new \SiteMaster\Plugins\Metric_pa11y\CommandHelper();
+    list($exitStatus, $output, $stderr) = $command_helper->exec($command, 25);
+    
+    return $output;
+}
+
+header('Content-Type: application/json');
+
+if (!isset($_GET['url'])) {
+    throw new \SiteMaster\Core\InvalidArgumentException('A URL is required', 400);
+}
+
+echo getResults($_GET['url'], $options);
--- www/templates/html/Metric.tpl.php
+++ www/templates/html/Metric.tpl.php
@@ -2,7 +2,8 @@
     Pa11y can't catch all accessibility errors. It'll catch many of them, but you should do manual checking as well.
 </p>
 <p>
-    Only distinct <?php echo $context->options['standard'] ?> errors are being reported.  Warnings and notices are being ignored and require manual checking.  To locate and fix errors on your page, drag this link to your browser bookmarks bar:
+    <?php $plugin_options = $context->getPlugin()->getOptions(); ?>
+    Only distinct <?php echo $plugin_options['standard'] ?> errors are being reported.  Warnings and notices are being ignored and require manual checking.  To locate and fix errors on your page, drag this link to your browser bookmarks bar:
     <a href="javascript:(function(){var e='<?php echo $base_url ?>plugins/metric_pa11y/widget.php';var t='<?php echo $base_url ?>www/js/vendor/jquery.js';var n=function(e,t){var n=document.createElement('script');n.onload=function(){n.onload=null;n.onreadystatechange=null;t.call(this)};n.onreadystatechange=function(){if(/^(complete|loaded)$/.test(this.readyState)===true){n.onreadystatechange=null;n.onload()}};n.src=e;if(document.head){document.head.appendChild(n)}else{document.getElementsByTagName('head')[0].appendChild(n)}};var r=function(){jQuery.getScript(e)};if(typeof jQuery==='undefined'){n(t,function(){r()})}else{r()}})();" onclick="alert('Drag this to your browser Bookmarks Bar to install.  Once installed, you can run the WCAG Bookmarklet over any page.'); return false;">SiteMaster Pa11y</a>, then click the 'HTML CodeSniffer' bookmark to run accessibility tests on any page.  Note that pages are checked at a mobile width.
 </p>
 
